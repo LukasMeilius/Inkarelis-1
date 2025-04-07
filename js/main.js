6cae0c4b070835a9a15cf3b662ea85f584a0690b
Luksssssssss <lukasis289@gmail.com>
2025-04-07 18:33:23 +0300

    Feat: add js logic for off screen and link timeout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 const menuIcon = document.querySelector('.menu-icon');
 const offScreenMenu = document.querySelector('.off-screen-menu');
+const openApieMus = document.querySelector('.open-apie-mus');
+const backToMain = document.querySelector('.back-to-main');
+const menuLinks = document.querySelectorAll('.off-screen-menu a');
+const logo = document.querySelector('.logo')
 
 // Reset scroll and hide menu when resizing to desktop
 window.addEventListener('resize', () => {
@@ -13,10 +17,40 @@ window.addEventListener('resize', () => {
 menuIcon.addEventListener('click', () => {
   const isActive = offScreenMenu.classList.toggle('active');
 
-  if (window.matchMedia("(max-width: 768px)").matches) {
+  if (window.matchMedia('(max-width: 768px)').matches) {
     document.body.style.overflow = isActive ? 'hidden' : 'auto';
   }
 });
+
+logo.addEventListener('click', (e) => {
+  if (window.matchMedia('(max-width: 768px)').matches) {
+    offScreenMenu.classList.remove('active');
+  } 
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  openApieMus.addEventListener('click', function (e) {
+    e.preventDefault();
+    offScreenMenu.classList.add('active-apie-mus');
+  });
+
+  backToMain.addEventListener('click', function (e) {
+    e.preventDefault();
+    offScreenMenu.classList.remove('active-apie-mus');
+  });
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    const href = this.getAttribute("href");
+    e.preventDefault();
+    setTimeout(() => {
+      window.location.href = href;
+    }, 300);
+  });
+});
+
 
 
 
